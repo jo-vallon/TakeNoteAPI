12ee68f8fee8fb000e7911449026f2aef66387ef
jo-vallon <jp.valp16@gmail.com>
2023-07-31 21:26:57 -0400

    FCT: Creation du controller de note

---
+++ TakeNoteAPI/TakeNoteAPI/Controllers/NoteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TakeNoteAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NoteController : ControllerBase
+    {
+        [HttpGet("GetTestResponse")]
+        public string GetTestResponse()
+        {
+            return "Response has been recieved";
+        }
+	}
+}
+
